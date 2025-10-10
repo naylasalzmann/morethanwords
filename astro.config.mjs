09463793bc06b1a2afc4f711d3cb41de4e589991
Nayla Salzmann <naylasalzmann@gmail.com>
2025-10-10 17:53:58 +0200

    fix: downgrade Tailwind CSS to v3 for Astro compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
-
+import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [
+    react(),
+    tailwind(),
+  ],
 });
